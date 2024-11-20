b4373263477c517c03c9da9791e85e469e39e64c
Joseph Arenas <professionaljarenas@gmail.com>
2024-11-20 22:30:46 +0800

    Unfinished
    Not finished. Will be finalized and polished soon. Bank Account Balance Tracker has problems on _str_() function. Will add an isolated branch for continuous testing and will be merged to main once done.

---
+++ Python/Bank Account Balance Tracker.py
@@ -0,0 +1,36 @@
+class BankAccount:
+  def __init__(self, account, balance):
+    self.account= account
+    self.balance= balance
+    
+  def deposit(self, input_Dep):
+    self.balance += input_Dep
+    return f"Deposit Input: {self.balance}. Current balance: {self.balance}"
+  
+  def withdraw(self, input_Withdraw):
+    if self.balance < input_Withdraw:
+      return f"Error: Insufficient Funds for Withdrawal! Current balance is {self.balance}"
+    elif self.balance >= input_Withdraw:
+      self.balance -= input_Withdraw
+      return f"Withdrew {input_Withdraw}php. Current balance is {self.balance}"
+    
+  def __str__(self):
+    return f"""Bank Account: {self.account}
+  Current Balance: {self.balance}
+  
+  Good Day {self.account}!
+  How may we help you today?"""
+  
+print("Please enter your Bank Account Name and your Current Balance.")
+account= input("Enter your Bank Account Name: ")
+balance= input("Enter your current balance: ")
+test= BankAccount()
+print(test)
+dec= input("\nWould you like to Withdraw or Deposit? (W / D): ")
+decision= dec.upper()
+if decision == "W":
+  BankAccount.withdraw(self, input_Withdraw)
+elif decision == "D":
+  BankAccount.deposit(self, input_Dep)
+else:
+  print("Error: Enter a valid input Mr. {self.account}")

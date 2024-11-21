5db08ce255f35c04baab0a29d5fcb85d88b043bb
Joseph Arenas <professionaljarenas@gmail.com>
2024-11-21 09:31:53 +0800

    Version 1
    First working version for Bank Account Balance Tracker.

--- Bank Account Balance Tracker.py
+++ Bank Account Balance Tracker.py
@@ -1,25 +1,26 @@
 class BankAccount:
   def __init__(self, account, balance):
     self.account= account
-    self.balance= balance
+    self.balance= float(balance)
     
   def deposit(self, input_Dep):
     self.balance += input_Dep
-    return f"Deposit Input: {self.balance}. Current balance: {self.balance}"
+    return f"Deposit Input: {input_Dep}. Current balance: {self.balance} php"
   
   def withdraw(self, input_Withdraw):
     if self.balance < input_Withdraw:
       return f"Error: Insufficient Funds for Withdrawal! Current balance is {self.balance}"
     elif self.balance >= input_Withdraw:
       self.balance -= input_Withdraw
-      return f"Withdrew {input_Withdraw}php. Current balance is {self.balance}"
+      return f"Withdrew {input_Withdraw}php. Current balance is {self.balance} php"
     
   def __str__(self):
-    return f"""Bank Account: {self.account}
-  Current Balance: {self.balance}
+    return f"""\nGood Day {self.account}!
+  How may we help you today?
   
-  Good Day {self.account}!
-  How may we help you today?"""
+  Personal Information
+  Bank Account: {self.account}
+  Current Balance: {self.balance} php"""
   
 print("Please enter your Bank Account Name and your Current Balance.")
 account= input("Enter your Bank Account Name: ")
@@ -31,9 +32,9 @@ dec= input("\nWould you like to Withdraw or Deposit? (W / D): ")
 decision= dec.upper()
 if decision == "W":
   input_Withdraw= float(input("Enter the amount you would like to withdraw: "))
-  test.withdraw(self, input_Withdraw)
+  print(test.withdraw(input_Withdraw))
 elif decision == "D":
-  input_Dep= float(input("Enter the amount you would like to withdraw: "))
-  test.deposit(self, input_Dep)
+  input_Dep= float(input("Enter the amount you would like to deposit: "))
+  print(test.deposit(input_Dep))
 else:
-  print("Error: Enter a valid input Mr. {self.account}")
+  print("Error: Enter a valid input {self.account}.")
